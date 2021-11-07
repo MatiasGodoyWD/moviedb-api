b0e56471030755b67455c64b742e4d2a7106c5a0
Matias Godoy <matiasjuliangodoy@gmail.com>
2021-11-07 17:55:19 -0300

    api key working

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("anda");
+const cargarPeliculas = async () => {
+  let data = await fetch(
+    "https://api.themoviedb.org/3/movie/550?api_key=4bf37a006026ff5c85b0ad03ea399347"
+  );
+  let response = await data.json();
+  console.log(response.genres[0].name);
+  return response;
+};
+
+cargarPeliculas();
